beacdb03cccd570d518037e8ebc3c0db0ef342db
Sujeet Kumar <vsujeet321@gmail.com>
2024-08-12 10:22:49 +0530

    add A.file

---
+++ A.java
@@ -0,0 +1,17 @@
+public class A {
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+    A  a1 = new A();
+        a1.A();
+        B();
+    }
+    void A (){
+        System.out.println("Method A");
+    }
+    static void B(){
+        System.out.println("Method B");
+    }
+    A(){
+        System.out.println("Constructor A");
+    }
+}
